0c180b852184ecaaa61580b067f5ea6483a168cd
Kitaps <paliedtke@uc.cl>
2022-10-11 09:31:18 -0300

    add get_category_range function
    search the obtained soup, find the pages ordered list and obtain the max page number

--- src/sodimac_data_getter.py
+++ src/sodimac_data_getter.py
@@ -1,16 +1,32 @@
+from xml.dom.minidom import AttributeList
 import requests
 from icecream import ic
 from bs4 import BeautifulSoup
 from aux_functions import load_categories
 
 categories = load_categories("sodimac")
-ic(categories)
+
+def get_page_range(soup):
+    # Find any of the page list elements on the site
+    pages_ordered_list = soup.find('ol', attrs = {'class': 'jsx-1794558402 jsx-1490357007'})
+    # ic(int(tuple(pages_ordered_list)[-1].text))
+    # Extract the last element from the obtained html element
+    #   To do this, we turn the element into a tuple and then obtain it's last element
+    last_element = tuple(pages_ordered_list)[-1]
+    # This element will have a text containing the last page name, which we want
+    number_name = last_element.text
+    # Finally we turn the number string into a number and return
+    return int(number_name)
+    
 
 if __name__ == "__main__":
 
-    url = 'https://sodimac.falabella.com/sodimac-cl/category/CATG10731/Materiales-de-Construccion?subdomain=sodimac&page=1&store=sodimac'
-    response = requests.get(url).content
-    soup = BeautifulSoup(response, 'html5lib')
-    # ic(soup.prettify())
-    
-    # https://www.geeksforgeeks.org/implementing-web-scraping-python-beautiful-soup/
+    for category in categories:
+        ic(category)    
+        category_url = f'https://sodimac.falabella.com/sodimac-cl/category/{category}?subdomain=sodimac&page=1&store=sodimac'
+        response = requests.get(category_url).content
+        soup = BeautifulSoup(response, 'html5lib')
+        # ic(soup.prettify())
+        ic(get_page_range(soup))
+        
+        # https://www.geeksforgeeks.org/implementing-web-scraping-python-beautiful-soup/
