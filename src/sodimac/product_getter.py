89a0331ebf9ae9eaea9f8a3482ccdf0d1f999ac1
Kitaps <paliedtke@uc.cl>
2022-11-03 17:33:56 -0300

    complete Product Class
    add parser to sodimac product getter
    fix minor errors
    add atributes to product

--- src/sodimac/product_getter.py
+++ src/sodimac/product_getter.py
@@ -1,4 +1,3 @@
-import time
 import json
 import requests
 from random import randint
@@ -35,25 +34,42 @@ def extract_page_data(soup):
     product_data = soup_json["props"]["pageProps"]["productData"]
     return product_data
 
+def parse_data(raw_data):
+    # Takes the obtained data and parses the useful parts to match the Product Object format
+    kwargs = {
+        "name": raw_data["name"],
+        "product_id": raw_data["id"],
+        "brand": raw_data["brandName"],
+        "description": raw_data["description"],
+        # raw_data["breadCrumb"], # Page data (link, category, ...
+        "sku": raw_data["currentVariant"],
+        "image_at": raw_data["variants"][0]["medias"][0]["url"],
+        "price": clean(raw_data["variants"][0]["prices"][0]["price"][0]),
+        "url": raw_data["shareIcons"][0]["url"],
+        "specifications": trim(raw_data["attributes"]["specifications"])}
+    return kwargs
+
+def trim(raw_specifications):
+    # Recives specifications as 
+    # [{'id': 'Marca', 'name': 'Marca', 'value': 'Mamut'},
+    # {'id': 'Largo_de_la_rosca', 'name': 'Largo de la rosca', 'value': '2 "'},
+    # {'id': 'Diámetro', 'name': 'Diámetro', 'value': '1/4 "'}]
+    # and resturns it as 
+    # {"Marca": "Mamut",
+    # "Largo_de_la_rosca": "2 "",
+    # "Diámetro": 1/4 ""}
+    specifications_dict = {entry["id"]: entry["value"] for entry in raw_specifications}
+    return specifications_dict
+
+def clean(price):
+    # Remove . and transform from str to int
+    return int(price[0].replace(".", ""))
 
 if __name__ == "__main__":
 
-    product_data = get_data_for(110316098)
-    # test_url = "https://www.falabella.com/falabella-cl/search?Ntt=110316098"
-    # request = requests.get(test_url)
-    # data_soup = BeautifulSoup(request.text, "lxml")
-    # soup_data = data_soup.find("script", attrs={'id': '__NEXT_DATA__'})
-    # soup_json = json.loads(soup_data.get_text())
-    # product_data = soup_json["props"]["pageProps"]["productData"]
-    ic(product_data["name"])
-    ic(product_data["id"])
-    ic(product_data["brandName"])
-    ic(product_data["description"])
-    ic(product_data["breadCrumb"]) # Page data (link, category, ...)
-    ic(product_data["currentVariant"])
-    ic(product_data["variants"][0]["medias"][0]["url"])
-    ic(product_data["attributes"]["specifications"])
-    # ic(product_data["name"])
+    product_data = get_data_for(110316112)
+    ready_data = parse_data(product_data)
+    ic(", ".join(ready_data.keys()))
 
-    with open("request_inputs&outputs/sodimac/product.json", 'w') as json_file:
-        json.dump(product_data, json_file)
+    # with open("request_inputs&outputs/sodimac/product.json", 'w') as json_file:
+    #     json.dump(product_data, json_file)
