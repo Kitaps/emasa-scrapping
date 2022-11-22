8fec08c18cc8cc2574f1c2a6a792492257675c0a
Kitaps <paliedtke@uc.cl>
2022-11-22 14:22:06 -0300

    update main
    add main2 function to work with autoplanets get_categories
    push to snowflake
    add autoplanet parser

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from random import randint
 from time import sleep
+from itertools import chain
 from icecream import ic
 import src.sodimac.product_getter as sodimac_getter
 import src.easy.product_getter as easy_getter
+import src.autoplanet.category_getter as autoplanet_getter
 from src.products import Product
 from src.database_hanlder import DBHandler
 from src.sku_getter import sonax_skus
 
 
 
-def main():
+def main1():
     errors = 0
     handler = DBHandler() # Create instance of database handler
     # We create two example products to try some things
@@ -24,6 +26,9 @@ def main():
                 case "easy":
                     raw_data = easy_getter.get_data_for(sku)
                     ready_data = easy_getter.parse_data(raw_data, sku)
+                case "autoplanet":
+                    print("Autoplanet is currently only working with get_category")
+                    continue
                 case _:
                     print(f"There is no store {store} registered.")
                     continue
@@ -40,5 +45,43 @@ def main():
     handler.insert_items()
     handler.execute_commands()
 
+def main2():
+    errors = 0
+    handler = DBHandler() # Create instance of database handler
+    autoplanet_products = dict()
+    current_soup = None
+    last_page_number = 0
+    for category in autoplanet_getter.categories:
+        autoplanet_products[category] = []
+        aux_generator = []
+        ic(category)
+
+        current_soup, last_page_number = autoplanet_getter.init_(category)
+        aux_generator = chain([], autoplanet_getter.extract_page_data(current_soup))
+
+        for page_number in range(1, last_page_number):
+            sleep(randint(1, 10))
+            try:
+                ic(page_number)
+                current_soup = autoplanet_getter.get_page_data(category, page_number)
+                current_soup_json = autoplanet_getter.extract_page_data(current_soup)
+                aux_generator = chain(aux_generator, current_soup_json) # Concatenate lists
+            except Exception as error:
+                print(type(error))
+                errors += 1
+                continue
+
+        ready_data_generator = map(lambda raw_data: autoplanet_getter.parse_data(raw_data), aux_generator)
+        product_list = ic(list(map(lambda rd: Product(**rd), ready_data_generator)))
+        handler.products.extend(product_list)
+        # # ic(handler.products)
+
+    handler.create_table("product")
+    handler.insert_items()
+    handler.execute_commands()
+
+
+
 if __name__ == "__main__":
-    main()
+    # main1()
+    main2()
--- src/aux_functions.py
+++ src/aux_functions.py
@@ -8,3 +8,13 @@ def load_categories(company_name):
     # Theese categorys are saved in the categories json of each company.
     with open(f"{company_name}_categories.json", 'r') as file:
         return json.load(file)
+
+def format_link(string):
+    # Turns string into a link, compatible with chrome
+    string = string.replace("(", "%28").replace(")", "%29")
+    string = string.replace("-", "_")
+    return string
+
+if __name__ == "__main__":
+    string = 'https://www.autoplanet.cl/producto/Filtro-de-Aceite-Mann-Filter-Jeep-(Cod.-Ref.-HU821X)-OEM-A6421840025/110177'
+    print(format_link(string))
